a511c77d190fd6a83f3c3b80c0947bea0ce49d53
mohmulla <125492083+mohmulla@users.noreply.github.com>
2023-07-08 17:30:53 +0300

    Create 33-toUpperString.c

---
+++ 33-toUpperString.c
@@ -0,0 +1,31 @@
+// program that finds if brackets are balanced
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{
+	char string[81] = { "fabbbbcbag" };
+	void toUpper(char string[]);
+
+	toUpper(string);
+	
+
+	printf("%s",string);
+
+
+	return 0;
+}
+
+void toUpper(char string[])
+{
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] = string[i] - 32;
+		}
+	}
+}
